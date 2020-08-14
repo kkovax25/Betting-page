9039fcfe0e3f31942b566fde6b1b38d651e020ae
Kovács Krisztián <kkovax94@gmail.com>
2020-08-14 16:02:09 +0200

    Materializecss setup

--- betting-page/src/App.js
+++ betting-page/src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    M.AutoInit();
+  })
+
+
   return (
     <div>
 
