3e121c0c13d0b5e34d55b5906a47ed111cd22735
Kovács Krisztián <kkovax94@gmail.com>
2020-08-14 16:10:29 +0200

    Added Redux

--- betting-page/src/App.js
+++ betting-page/src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   useEffect(() => {
@@ -10,9 +12,10 @@ function App() {
 
 
   return (
-    <div>
-
-    </div>
+    <Provider store={store}>
+      <div>
+      </div>
+    </Provider>
   );
 }
 
