adddfc5e7270a828c0b325e6c1727b7de36cbf5d
Kovács Krisztián <kkovax94@gmail.com>
2020-08-29 19:12:06 +0200

    Created register

--- betting-page-backend/server.js
+++ betting-page-backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./db');
+const colors = require('colors');
 
 const app = express();
+
+// Connect to db
+connectDB();
+
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,4 +18,4 @@ app.use('/api/auth', require('./routes/auth'))
 
 
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`.yellow.bold.italic))
